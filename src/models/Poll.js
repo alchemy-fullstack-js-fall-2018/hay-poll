ede4b8cd4530e97b911c44c5cc82e8668a0382c8
Alex Rankin <alex.c.rankin@gmail.com>
2018-12-04 10:09:31 -0800

    initial & boilerplate

---
+++ src/models/Poll.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const pollSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  candidates: [{
+    name: {
+      type: String,
+      required: true
+    }
+  }]
+});
---
+++ src/routes/api/polls.js
@@ -0,0 +1,4 @@
+import { Router } from 'express';
+import Poll from '../../models/Poll';
+
+export default Router()
