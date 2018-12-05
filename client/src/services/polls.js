7525db0c314a503ff3e73299344001b4da12b8b0
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-12-05 10:17:02 -0800

    post poll and get results test passing

--- client/src/services/polls.js
+++ client/src/services/polls.js
@@ -1,22 +1,22 @@
 import { get, post } from './request';
 
 export const getPolls = () => {
-  return get('api/polls');
+  return get('/api/polls');
 };
 
 export const getPoll = id => {
-  return get(`api/polls/${id}`);
+  return get(`/api/polls/${id}`);
 };
 
 export const postPoll = poll => {
-  return post('api/polls', poll);
+  return post('/api/polls', poll);
 };
 
 export const getResults = id => {
-  return get(`api/polls/${id}/results`);
+  return get(`/api/polls/${id}/results`);
 };
 
 export const postVote = (id, vote) => {
-  return post(`api/polls/${id}/votes`, vote);
+  return post(`/api/polls/${id}/votes`, vote);
 };
 
--- client/src/services/polls.test.js
+++ client/src/services/polls.test.js
@@ -7,21 +7,21 @@ import mockPostVoteJson from './fixtures/postVote.json';
 
 jest.mock('./request.js', () => ({
   get: url => {
-    if(url.startsWith('api/polls')) {
-      return Promise.resolve(mockPollsJson);
-    } else if(url.startsWith('api/polls/')) {
-      return Promise.resolve(mockPollJson);
-    } else if(url.startsWith('api/polls/') && url.endsWith('results')) {
+    if(url.startsWith('/api/polls/') && url.endsWith('results')) {
       return Promise.resolve(mockResultsJson);
+    } else if(url.startsWith('/api/polls/')) {
+      return Promise.resolve(mockPollJson);
+    } else if(url.startsWith('/api/polls')) {
+      return Promise.resolve(mockPollsJson);
     } else {
       return Promise.reject({ error: '404' });
     }
   },
   post: url => {
-    if(url.startsWith('api/polls')) {
-      return Promise.resolve(mockPostPollJson);
-    } else if(url.startsWith('api/polls/')) {
+    if(url.startsWith('/api/polls/')) {
       return Promise.resolve(mockPostVoteJson);
+    } else if(url.startsWith('/api/polls')) {
+      return Promise.resolve(mockPostPollJson);
     } else {
       return Promise.reject({ error: '404' });
     }
@@ -29,18 +29,42 @@ jest.mock('./request.js', () => ({
 }));
 
 describe('polls api', () => {
-  it('gets a list of poll', () => {
-    getPolls()
+  it('gets a list of poll from database', () => {
+    return getPolls()
       .then(polls => {
         expect(polls).toHaveLength(2);
       });
   });
 
-  it('gets a single poll', () => {
-    getPoll()
+  it('gets a single poll from database', () => {
+    const id = '5c0729019ce98c6d4af5c534';
+
+    return getPoll(id)
       .then(poll => {
         expect(poll).toHaveLength(1);
       });
+  });
+
+  it('gets results for a choice that shows the number of times it was voted for from database', () => {
+    const id = '5c0729019ce98c6d4af5c535';
+
+    return getResults(id)
+      .then(results => {
+        expect(results).toContainEqual({ _id: expect.any(String), count: expect.any(Number) });
+      });
+  });
+
+  it('posts a poll to database', () => {
+    const poll = {
+      title: 'What pet is the cutest',
+      options: [{ choice: 'a badger' }, { choice: 'a rat' }, { choice: 'a snake' }]
+    };
+    return postPoll(poll)
+      .then(poll => expect(poll).toEqual({
+        ...poll,
+        __v: expect.any(Number),
+        _id: expect.any(String)
+      }));
   });
 });
 
