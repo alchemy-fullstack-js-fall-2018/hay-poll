df81c678e98d8c911b4f22130530028df4b2ff96
Jack <ajackofsometrades@gmail.com>
2018-12-05 23:59:23 -0800

    moving routes

--- server/resources/index.js
+++ server/resources/index.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import polls from './polls/routes';
 import users from './users/routes';
+import votes from './votes/routes';
 
 const resources = [
-  polls, users
+  polls, users, votes
 ];
 
 export default Router().use('/', ...resources);
--- server/resources/polls/routes.js
+++ server/resources/polls/routes.js
@@ -11,21 +11,6 @@ export default Router()
       .catch(next);
   })
 
-  .post('/polls/:id/votes', requireAuth, (req, res, next) => {
-    const { poll, selection } = req.body;
-    Vote.create({ poll, selection })
-      .then(vote => res.json(vote))
-      .catch(next);
-  })
-
-  .get('/polls/:id/results', (req, res, next) => {
-    const { id } = req.params;
-    Poll.findById(id)
-      .then(poll => poll.results())
-      .then(results => res.json(results))
-      .catch(next);
-  })
-
   .get('/polls/:id', (req, res, next) => {
     const { id } = req.params;
     Poll.findById(id)
